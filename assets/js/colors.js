64c965a1232c56fb5d0c7c995dbb03d2c9866dfe
VodeniZeko <salexy.es@gmail.com>
2020-01-18 02:36:17 -0800

    updates

---
+++ assets/js/colors.js
@@ -0,0 +1,28 @@
+// const body = document.querySelector("body");
+// const logo = document.querySelector("#logo");
+
+// window.scroll(0, 100);
+
+// function checkVisible(elm, eval) {
+//   eval = eval || "visible";
+//   var vpH = $(window).height(), // Viewport Height
+//     st = $(window).scrollTop(), // Scroll Top
+//     y = $(elm).offset().top,
+//     elementHeight = $(elm).height();
+
+//   if (eval == "visible") return y < vpH + st && y > st - elementHeight;
+//   if (eval == "above") return y < vpH + st;
+// }
+
+// // const logo = document.querySelector("#logo");
+// // console.log(logo);
+// // window.onscroll = function() {
+// //   "use strict";
+// //   if (document.body.scrollTop >= 2000) {
+// //     logo.classList.add("white");
+// //     logo.classList.remove("#logo");
+// //   } else {
+// //     logo.classList.remove("white");
+// //     logo.classList.add("#logo");
+// //   }
+// // };
--- assets/js/g.js
+++ assets/js/g.js
@@ -1,28 +1,66 @@
-// $(window).scroll(function() {
-//     if (checkVisible($('.text-muted'))) {
-//     	$('.item').css("background", 'fff');
-//         $('.item').css("opacity",.4);
-//         $('.item').css('transition', '1.5s');
-//         $('.a, .b, .c, .d, .e, .f, .g, .h').css('color', 'transparent');
-//         $('.a, .b, .c, .d, .e, .f, .g, .h').css('transition', '.5s');
-//     } else {
-//         $('.item').css("background", '#f2f2f2');
-//         $('.item').css("opacity",1);
-//         $('.item').css('transition', '1.5s');
-//         $('.a, .b, .c, .d, .e, .f, .g, .h').css('color', '#989898');
-//         $('.a, .b, .c, .d, .e, .f, .g, .h').css('transition', '.5s');
+$(document).scroll(function() {
+  if (checkVisible($(".projects"))) {
+    $(".projects").css("background", "#000");
+    $(".projects").css("transition", "1s");
+    $(".hidden").css("background", "#000");
+    $(".hidden").css("transition", "1s");
+    $(".w").css("color", "#fff");
+    $(".w").css("color", "1s");
+    $(".wp").css("color", "#fff");
+    $(".wp").css("color", "1s");
+    $(".two").css("background", "#000");
+    $(".two").css("transition", "1s");
+    $(".one").css("background", "#000");
+    $(".one").css("transition", "1s");
+    $(".card").css("transform", "scale(1)");
+    $(".card").css("transition", "2s");
+  } else {
+    $(".projects").css("background", "#fff");
+    $(".projects").css("transition", "1s");
+    $(".hidden").css("background", "#fff");
+    $(".hidden").css("transition", "1s");
+    $(".w").css("color", "#fff");
+    $(".w").css("color", "1s");
+    $(".wp").css("color", "#fff");
+    $(".wp").css("color", "1s");
+    $(".two").css("background", "#fff");
+    $(".two").css("transition", "1s");
+    $(".one").css("background", "#fff");
+    $(".one").css("transition", "1s");
+    $(".card").css("transform", "scale(0.5)");
+    $(".card").css("transition", "2s");
+  }
+});
+$(document).scroll(function() {
+  if (checkVisible($(".btn"))) {
+    $(".four").css("background", "#fff");
+    $(".four").css("transition", "1s");
+    $("input").css("background", "#fff");
+    $("input").css("transition", "1s");
+    $("textarea").css("background", "#fff");
+    $("textarea").css("transition", "1s");
+  } else {
+    $(".four").css("background", "#000");
+    $(".four").css("transition", "1s");
+    $("input").css("background", "#000");
+    $("input").css("transition", "1s");
+    $("textarea").css("background", "#000");
+    $("textarea").css("transition", "1s");
+  }
+});
+$(document).scroll(function() {
+  if (checkVisible($(".viewport"))) {
+    $(".projects").css("background", "#000");
+  }
+});
 
-//     }
-// });
+function checkVisible(elm, eval) {
+  eval = eval || "visible";
+  var vpH = $(window).height(), // Viewport Height
+    st = $(window).scrollTop(), // Scroll Top
+    y = $(elm).offset().top,
+    elementHeight = $(elm).height();
 
-// function checkVisible( elm, eval ) {
-//     eval = eval || "visible";
-//     var vpH = $(window).height(), // Viewport Height
-//         st = $(window).scrollTop(), // Scroll Top
-//         y = $(elm).offset().top,
-//         elementHeight = $(elm).height();
-    
-//     if (eval == "visible") return ((y < (vpH + st)) && (y > (st - elementHeight)));
-//     if (eval == "above") return ((y < (vpH + st)));
-// }
-
+  if (eval == "visible") return y < vpH + st && y > st - elementHeight;
+  if (eval == "above") return y < vpH + st;
+}
