3c2832ebc0c0905d9a31af8dca09d3ca53d45ce1
VodeniZeko <salexy.es@gmail.com>
2020-07-17 17:51:29 -0700

    numbers api

--- assets/js/numbersAPI.js
+++ assets/js/numbersAPI.js
@@ -1,46 +1,63 @@
 // NUMBERS API NUMBER API //
 
-$(document).ready(function() {
-  var today = new Date();
-  var dd = Number(today.getDate());
-  var mm = Number(today.getMonth());
-  var obj = {};
-  $.ajax({
-    url: `http://numbersapi.com/${mm}/${dd}/date`,
-    success: function(result) {
-      obj = result;
-      $("#numbersText").html(obj);
-    }
-  });
-  myInputBtn = $("#my-input-btn");
-  myInputBtn.on("click", function() {
-    var value = document.getElementById("my-input").value;
+// $(document).ready(function() {
+//   $.ajax({
+//     url: `https://1kfit61c36.execute-api.us-east-2.amazonaws.com/production/1990/year`,
+//     headers: {
+//       accessKey: "AKIAUQETXRJDBBF4HRG2",
+//       secretKey: "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"
+//     },
+//     success: function(res) {
+//       console.log(res);
+//     },
+//     error: function(xhr, ajaxOptions, thrownError) {
+//       //Add these parameters to display the required response
+//       alert(xhr.status);
+//       alert(xhr.responseText);
+//     }
+//   });
+// });
 
-    if (value) {
-      date = value.split(".");
-      day = date[0].replace(/ /g, "");
-      month = date[1].replace(/ /g, "");
-      year = date[2].replace(/ /g, "");
-      md = {};
-      ye = {};
-      $.ajax({
-        url: `http://numbersapi.com/${month}/${day}/date`,
-        success: function(x) {
-          md = x;
-          $("#month-day-text").html(md);
-        }
-      }),
-        $.ajax({
-          url: `http://numbersapi.com/${year}/year`,
-          success: function(y) {
-            ye = y;
-            $("#year-text").html(ye);
-          }
-        });
-    } else {
-      alert("Click on the 📅 (calendar) and choose a date first. ");
-    }
-  });
-});
+//   var today = new Date();
+//   var dd = Number(today.getDate());
+//   var mm = Number(today.getMonth());
+//   var obj = {};
+//   $.ajax({
+//     url: `https://1kfit61c36.execute-api.us-east-2.amazonaws.com/production/${mm}/${dd}/date`,
+//     success: function(result) {
+//       obj = result;
+//       $("#numbersText").html(obj);
+//     }
+//   });
+//   myInputBtn = $("#my-input-btn");
+//   myInputBtn.on("click", function() {
+//     var value = document.getElementById("my-input").value;
+
+//     if (value) {
+//       date = value.split(".");
+//       day = date[0].replace(/ /g, "");
+//       month = date[1].replace(/ /g, "");
+//       year = date[2].replace(/ /g, "");
+//       md = {};
+//       ye = {};
+//       $.ajax({
+//         url: `https://1kfit61c36.execute-api.us-east-2.amazonaws.com/production/${month}/${day}/date`,
+//         success: function(x) {
+//           md = x;
+//           $("#month-day-text").html(md);
+//         }
+//       }),
+//         $.ajax({
+//           url: `https://1kfit61c36.execute-api.us-east-2.amazonaws.com/production/${year}/year`,
+//           success: function(y) {
+//             ye = y;
+//             $("#year-text").html(ye);
+//           }
+//         });
+//     } else {
+//       alert("Click on the 📅 (calendar) and choose a date first. ");
+//     }
+//   });
+// });
 
 // NUMBERS API NUMBER API  ENDS//
